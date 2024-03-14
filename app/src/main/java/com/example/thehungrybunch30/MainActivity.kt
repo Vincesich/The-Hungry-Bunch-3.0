a710e165a4e2abee7700792aee8a66c48b4f15f1
Vincesich <82143726+Vincesich@users.noreply.github.com>
2024-03-14 12:32:02 +0300

    chaged fragment to fragment container

--- app/src/main/java/com/example/thehungrybunch30/MainActivity.kt
+++ app/src/main/java/com/example/thehungrybunch30/MainActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -13,23 +14,24 @@ import com.example.thehungrybunch30.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.title = "The Hungry Bunch"
         val actionBar = supportActionBar
         if (actionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        navHostFragment  = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
+        as NavHostFragment
+        navController = navHostFragment.navController
 
         val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
