30342cf5e30141b1c477a7ca43e865e2e15fdce3
Vincesich <158569050+Vincewekesa@users.noreply.github.com>
2024-03-15 15:06:43 +0300

    Added a example menu item

--- app/src/main/java/com/example/thehungrybunch30/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/thehungrybunch30/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
 }
