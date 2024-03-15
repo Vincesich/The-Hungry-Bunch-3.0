16f11ac68d75a30f8f3feacbf477a959fd7e3a23
Vincesich <158569050+Vincewekesa@users.noreply.github.com>
2024-03-15 14:17:44 +0300

    configured a customer toolbar from material 3

--- app/src/main/java/com/example/thehungrybunch30/MainActivity.kt
+++ app/src/main/java/com/example/thehungrybunch30/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.thehungrybunch30
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.thehungrybunch30.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,8 +20,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(findViewById(R.id.topAppBar))
 
+        setSupportActionBar(findViewById(R.id.topAppBar))
 
         navHostFragment  = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
         as NavHostFragment
@@ -32,13 +31,12 @@ class MainActivity : AppCompatActivity() {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu) // Inflate the menu resource containing the additional elements
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
